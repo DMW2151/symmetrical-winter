f142a5019424acc6cfa9d1b29e7b801a1c6c081c
Dustin Wilson <dmw2151@columbia.edu>
2021-10-21 00:06:19 -0400

    add alpine

--- terraform/modules/chef/chef_server_instances.tf
+++ terraform/modules/chef/chef_server_instances.tf
@@ -18,6 +18,8 @@ resource "null_resource" "wait_for_chef_init" {
     command     = <<-EOF
     set -x -Ee -o pipefail;
 
+    apk update && apk add aws-cli
+    
     export AWS_DEFAULT_REGION="${var.default_region}"
     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile default
     aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile default
